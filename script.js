0426f247210489e9abee045ec07e8041c4cd33f6
shashi-123jha <ranjanshashi854105@gmail.com>
2024-06-11 13:17:46 +0530

    crousal img commit

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
 
-next.addEventListener('click',function(){
-    let items = documents.querySelectorAll('.item')
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
     document.querySelector('.slide').appendChild(items[0])
+})
+
+// yha pe maximum 6 page hi use krna hai...
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) 
 })
